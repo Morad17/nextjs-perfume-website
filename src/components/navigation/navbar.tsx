0415c459a75645c0176af2b0660ca7f8144d0b25
Morad17 <morad123@hotmail.co.uk>
2025-11-21 21:26:13 +0000

    update

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -2,22 +2,20 @@ import Link from "next/link";
 import "./navbar.scss";
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <div className="logo"></div>
-      <ul className="nav-group">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/products">Products</Link>
-        </li>
-        <li>
-          <Link href="/">Shop</Link>
-        </li>
-        <li>
-          <Link href="/">Company</Link>
-        </li>
-      </ul>
-    </div>
+    <ul className="navbar">
+      <li>
+        <Link href="/">Company</Link>
+      </li>{" "}
+      <li>
+        <Link href="/">Shop</Link>
+      </li>
+      <li>
+        <Link href="/products">Products</Link>
+      </li>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <div className="logo">PS</div>
+    </ul>
   );
 }
