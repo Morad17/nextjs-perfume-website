49c6c7bb61cd442a9f969720d91f77f9a1a86244
Morad17 <morad123@hotmail.co.uk>
2026-04-21 22:09:51 +0100

    seeding prisma table, added scss mixins and stripe functionality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
-import { PrismaClient } from "../src/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../src/generated/prisma/client";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 const products = [
   {
